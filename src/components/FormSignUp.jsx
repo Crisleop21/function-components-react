69ee991a51b1298907bb49bd2f5470690427030a
Crisleop21 <leonelporoj@gmail.com>
2023-06-08 00:34:50 -0600

    final curse

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,64 +1,145 @@
-import { useState, useEffect} from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Switch from "@mui/material/Switch";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
+import { useState } from "react"
+import Button from "@mui/material/Button"
+import TextField from "@mui/material/TextField"
+import Switch from "@mui/material/Switch"
+import FormGroup from "@mui/material/FormGroup"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import { FormControl } from "@mui/material"
 
-function FormSignUp() {
+function FormSignUp({ handleSubmit }) {
 	const [name, setName] = useState("")
-	useEffect(() => {
-		console.log("Name cambio: ", name)
-	}, [name])
+	const [lastName, setLastName] =
+		useState("")
+	const [email, setEmail] = useState("")
+	const [prom, setProm] = useState(true)
+	const [nov, setNov] = useState(false)
+
+	const [errors, setErrors] = useState({
+		name: {
+			error: false,
+			message:
+				"Deben ser al menos 3 caracteres",
+		},
+	})
+
+	function validarNombre(nombre) {
+		if (nombre.length >= 3) {
+			return {
+				name: {
+					error: false,
+					message: "",
+				},
+			}
+		} else {
+			return {
+				name: {
+					error: true,
+					message:
+						"Deben ser al menos 3 caracteres",
+				},
+			}
+		}
+	}
 
 	return (
-		<form>
-			<TextField 
-				id="name" 
-				label="Nombre" 
-				variant="outlined" 
+		<form
+			onSubmit={(e) => {
+				e.preventDefault()
+				handleSubmit({
+					name,
+					lastName,
+					email,
+					prom,
+					nov,
+				})
+			}}
+		>
+			<TextField
+				id="name"
+				label="Nombre"
+				variant="outlined"
 				fullWidth
 				margin="normal"
-				onChange={(e) => {
-					console.log(e.target.value)
+				onChange={(e) =>
 					setName(e.target.value)
+				}
+				value={name}
+				error={errors.name.error}
+				helperText={
+					errors.name.error
+						? errors.name.message
+						: ""
+				}
+				onBlur={(e) => {
+					setErrors(
+						validarNombre(
+							e.target.value
+						)
+					)
 				}}
-				value={ name }
 			/>
-			<TextField 
-				id="lastName" 
-				label="Apellidos" 
+			<TextField
+				id="lastName"
+				label="Apellidos"
 				variant="outlined"
 				fullWidth
 				margin="normal"
+				value={lastName}
+				onChange={(e) =>
+					setLastName(e.target.value)
+				}
 			/>
-			<TextField 
-				id="email" 
-				label="Email" 
+			<TextField
+				id="email"
+				label="Email"
 				variant="outlined"
 				fullWidth
 				margin="normal"
+				value={email}
+				onChange={(e) =>
+					setEmail(e.target.value)
+				}
 			/>
 			<FormGroup>
-				<FormControlLabel 
+				<FormControlLabel
 					control={
-						<Switch defaultChecked />
+						<Switch
+							checked={prom}
+							onChange={(e) =>
+								setProm(
+									e.target.checked
+								)
+							}
+						/>
 					}
-					label="Promociones" 
+					label="Promociones"
 				/>
-			
-				<FormControlLabel 
+
+				<FormControlLabel
 					control={
-						<Switch defaultChecked />
+						<Switch
+							checked={nov}
+							onChange={(e) =>
+								setNov(e.target.checked)
+							}
+						/>
 					}
 					label="Novedades"
 				/>
 			</FormGroup>
-			<Button variant="contained">
+			<Button
+				variant="contained"
+				type="submit"
+			>
 				Registrarse
 			</Button>
 		</form>
 	)
+}
+
+function CrearHook() {
+	const [a, setA] = useState("a")
+	return <></>
 }
 
 export default FormSignUp
